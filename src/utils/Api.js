1c85821185feb8b3f3919c03699127eca0dccd9b
amatt <amattson84@gmail.com>
2021-04-09 11:14:55 -0700

    updated .get path

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -9,7 +9,8 @@ export default {
     return axios.get("https://api.thecatapi.com/v1/images/search");
   },
   getCatsOfBreed: function(breed) {
-    return axios.get("https://api.thecatapi.com/images/search?{{selected_breed.id}}");
+    return axios.get("https://api.thecatapi.com/v1/breeds/search}");
+
   },
   getBaseBreedsList: function() {
     return axios.get("https://api.thecatapi.com/v1/breeds");
