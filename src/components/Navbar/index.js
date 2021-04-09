2a0ffaaa8002f2f996b78f8fca11ec3a2a70f7e6
amatt <amattson84@gmail.com>
2021-04-09 13:29:17 -0700

    Updated page title

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        LoveMycat
       </Link>
       <div>
         <ul className="navbar-nav">
